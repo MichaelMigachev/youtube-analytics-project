6b62ef49e6a4d52310800eeb6c571039054f5bc6
MichaelMigachev <migachev21@gmail.com>
2023-09-18 13:56:33 +0300

    Выполнил homework1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
-
+import json
+import os
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
+        self.__channel_id = channel_id
+        views = self.channel_info["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.get_service().channels().list(id=self.channel_id,
+                                                     part="snippet,statistics").execute()
+
