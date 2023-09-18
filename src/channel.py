5096677a41f3f563fee05908ced854610fdb3b3c
MichaelMigachev <migachev21@gmail.com>
2023-09-18 19:39:31 +0300

    доработал вывод

--- src/channel.py
+++ src/channel.py
@@ -8,10 +8,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала.
         Дальше все данные будут подтягиваться по API."""
-        self.__channel_id = channel_id
-        views = self.channel_info["items"][0]["statistics"]["viewCount"]
+        self.youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+        self.id = channel_id
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.get_service().channels().list(id=self.channel_id, part="snippet,statistics").execute()
-
+        # channel = self.get_service().channels().list(id=self.channel_id, part="snippet,statistics").execute()
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
